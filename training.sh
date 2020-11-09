c97f33fd769df8fa31dc7abed11797aa1acb3bfc
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-11-09 17:35:40 +0900

    Update training.sh

--- training.sh
+++ training.sh
@@ -1,6 +1,6 @@
 PIPELINE_CONFIG_PATH='/tf_ssd/tod/train_models/research/object_detection/samples/configs/ssd_inception_v2_coco.config'
 
-MODEL_DIR='/tf_ssd/tod/save_models/coco_test'
+MODEL_DIR='/tf_ssd/save_models/coco_test'
 
 #vBox 의 경우 저장공간도 부족 !!
 
@@ -14,7 +14,7 @@ SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 
 #vBox 는 gpu 가 없기 때문에 다음의 변수 지정이 필요하다고 함
 
-export TF_CPP_MIN_LOG_LEVEL=2
+#export TF_CPP_MIN_LOG_LEVEL=2
 
 python object_detection/model_main.py --pipeline_config_path=${PIPELINE_CONFIG_PATH} --model_dir=${MODEL_DIR} --num_train_steps=${NUM_TRAIN_STEPS} --num_eval_steps=${NUM_EVAL_STEPS} --checkpoint_dir=${PRE_TRAIN} --sample_1_of_n_eval_examples=$SAMPLE_1_OF_N_EVAL_EXAMPLES --num_clones=1 --ps_tasks=1
 
