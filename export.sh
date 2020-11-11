a4326128a4f38fb95eaa47aadd8e80020e2cad6a
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-11-11 13:19:17 +0900

    Update export.sh

--- export.sh
+++ export.sh
@@ -2,7 +2,7 @@ INPUT_TYPE=image_tensor
 
 PIPELINE_CONFIG_PATH='/tf_ssd/tod/export_models/research/object_detection/samples/configs/ssd_inception_v2_coco.config'
 
-TRAINED_CKPT_PREFIX='/tf_ssd/tod/save_models/coco_test/model.ckpt-20'
+TRAINED_CKPT_PREFIX='/tf_ssd/save_models/coco_test/model.ckpt-20'
 
 EXPORT_DIR='/tf_ssd/tod/pbfiles'
 
