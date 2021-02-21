f32c504c1c0e34ecf880086e8688bf314c46b1ef
Kate <49902595+katebrighteyes@users.noreply.github.com>
2021-02-22 06:35:10 +0900

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,10 +1,17 @@
-INPUT_TYPE=image_tensor
+export PYTHONPATH=$PYTHONPATH:/tf_ssd/tod/export_models/research:/tf_ssd/tod/export_models/research/slim
+
+protoc object_detection/protos/*.proto --python_out=.
+
+python object_detection/builders/model_builder_test.py
 
 PIPELINE_CONFIG_PATH='/tf_ssd/tod/export_models/research/object_detection/samples/configs/ssd_inception_v2_coco.config'
 
-TRAINED_CKPT_PREFIX='/tf_ssd/save_models/coco_test/model.ckpt-20'
+TRAINED_CKPT_PREFIX='/tf_ssd/save_models/coco_test/model.ckpt-2000'
 
-EXPORT_DIR='/tf_ssd/tod/pbfiles'
+INPUT_TYPE='image_tensor'
+
+EXPORT_DIR='/tf_ssd/pbfiles'
 
 python object_detection/export_inference_graph.py --input_type=${INPUT_TYPE} --pipeline_config_path=${PIPELINE_CONFIG_PATH} --trained_checkpoint_prefix=${TRAINED_CKPT_PREFIX} --output_directory=${EXPORT_DIR}
+
 
